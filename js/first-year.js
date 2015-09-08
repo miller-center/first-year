1626e28c0fdf1330061727c18a661598f9697e7b
Matthew Stephens <mattbastard@gmail.com>
2015-09-08 11:16:18 -0400

    Last direct changes to master branch
    Updating project organization in preparation for shared workflow.
    
    From now on, all features should be developed in their own branches
    and merged into master after review.

--- js/first-year.js
+++ js/first-year.js
@@ -1,1 +1,38 @@
 $(document).foundation();
+
+$(document).ready(function() {
+  // re-initialize Foundation after responsive image loading
+  $(document).foundation('interchange', 'reflow');
+
+  // give page a nudge if loading anywhere other than top
+  var currentPosition = $(window).scrollTop();
+  if (currentPosition > 0) {
+    $(window).scrollTop(currentPosition + 1 );
+  }
+
+  // navigation header expand/contract
+  $('#navigation-container').addClass('shrinkMenu');
+
+  $(window).scroll(function() {
+
+    var threshold = $('#hero-container').height() * 0.01;
+    if ($(this).scrollTop() <= threshold) { 
+      $('#navigation-container').addClass('shrinkMenu');
+      $('#navigation-container').removeClass('expandMenu');
+    }
+    else {
+      // $('#navigation-container').show(); 
+      $('#navigation-container').addClass('expandMenu');
+      $('#navigation-container').removeClass('shrinkMenu');
+    }
+
+    var belowHero = $('section.hero').scrollTop() + $('section.hero').height() - $('#navigation-container').height();
+    if ($(this).scrollTop() >=  belowHero ) {
+      $('#navigation-container').addClass('belowHero');
+    } else {
+      $('#navigation-container').removeClass('belowHero');
+    }
+
+  });
+
+});
