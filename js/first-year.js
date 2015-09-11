c784be447bd362151e030fb8191fa7d181740a1b
Matthew Stephens <mattbastard@gmail.com>
2015-09-11 18:43:47 -0400

    Properly functioning Nav menus!!
    The trick here was concealing (on md and lg) some buttons near the
    existing dropdown list.  The mobile menu will render those as a
    dropdown, and we suppress the lg dropdown, b/c it was off-canvas and
    required an extra click to discover.  This version should be more
    mobile-friendly
    
    The JS work is to correct a known issue with the Topbar module, and
    you should now be able to click/tap outside the menu to get it to
    collapse.  This was not happening before.
    
    Much styling work to do, but functionality appears good.

--- js/first-year.js
+++ js/first-year.js
@@ -1,4 +1,10 @@
-$(document).foundation();
+$(document).foundation({
+  topbar : {
+    custom_back_text: false,
+    is_hover: true,
+    mobile_show_parent_link: false
+  }
+});
 
 $(document).ready(function() {
   // re-initialize Foundation after responsive image loading
@@ -9,6 +15,23 @@ $(document).ready(function() {
   if (currentPosition > 0) {
     $(window).scrollTop(currentPosition + 1 );
   }
+
+  // when clicking outside of navigation header, collapse menus
+  $(document).mouseup(function (e){
+      var container = $(".top-bar");
+      if (!container.is(e.target) // if the target of the click isn't the container...
+          && container.has(e.target).length === 0){ // ... nor a descendant of the container
+          container.removeClass('expanded');
+      }
+  });
+  // same for tap (mobile)
+  $(document).on({'touchend': function (e){
+      var container = $(".top-bar");
+      if (!container.is(e.target) // if the target of the click isn't the container...
+          && container.has(e.target).length === 0){ // ... nor a descendant of the container
+          container.removeClass('expanded');
+      }
+    } });
 
   // navigation header expand/contract
   $('#navigation-container').addClass('shrinkMenu');
