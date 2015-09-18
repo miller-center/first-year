3995883c3905205b694b4e852ecaed43ed2d0a78
Matthew Stephens <mattbastard@gmail.com>
2015-09-18 16:55:46 -0400

    Adding News ticker, with 5s rotation
    This is using JQuery and I have added some custom functions to track
    the size of the viewport, because there is no need to have this
    Javascript running on devices where the entire news feed is not
    displayed.  Note the use of console logging statemtents, to be
    deleted at a later date.

--- js/first-year.js
+++ js/first-year.js
@@ -1,8 +1,30 @@
 $(document).foundation();
+window.FirstYear = {}; // project globals
 
 $(document).ready(function() {
   // re-initialize Foundation after responsive image loading
   $(document).foundation('interchange', 'reflow');
+
+  // test for screen size, for responsive JS execution
+  FirstYear.viewportSmall = $(window).isSmallDevice();
+  FirstYear.viewportMedium = $(window).isMediumDevice();
+  FirstYear.viewportLarge = $(window).isLargeDevice();
+
+  // reset values upon change in window size
+  $(window).resize(function () {
+    if (FirstYear.viewportSmall !== $(window).isSmallDevice()) {
+      FirstYear.viewportSmall = $(window).isSmallDevice();  
+      console.log('re-setting viewportSmall');
+    }
+    if (FirstYear.viewportMedium !== $(window).isMediumDevice()) {
+      FirstYear.viewportMedium = $(window).isMediumDevice();  
+      console.log('re-setting viewportMedium');
+    }
+    if (FirstYear.viewportLarge !== $(window).isLargeDevice()) {
+      FirstYear.viewportLarge = $(window).isLargeDevice();  
+      console.log('re-setting viewportLarge');
+    }
+  });
 
   // give page a nudge if loading anywhere other than top
   var currentPosition = $(window).scrollTop();
@@ -36,4 +58,77 @@ $(document).ready(function() {
 
   });
 
+  //
+  // News Ticker Animation
+  //
+
+  // rotate through the list of 'The Latest' headlines
+  var listItems = $('#the-latest ul li:not(.label)');
+      length = listItems.length;
+      current = 0;
+      timeout = 5000;
+
+  function changeNewsItem() {
+    listItems.eq(current++).fadeOut(300, function() {
+      if (current === length) {
+        current = 0;
+      }
+      listItems.eq(current).fadeIn(300);
+
+    });
+    setTimeout(changeNewsItem, timeout);
+  }
+
+
+  // if window is large enough, active newsfeed
+  try {
+    var isSmallDevice = $(window).isSmallDevice();
+    if(! isSmallDevice) {
+      setTimeout(changeNewsItem, timeout);
+    } else {
+      console.log('news feed disabled for small device');
+    }
+  }
+  catch(err) {
+    console.log(err);
+  }
+
 });
+
+//
+// utilities
+//
+
+// sniff window and detect flag indicating small device
+jQuery.fn.isSmallDevice = function() {
+  var floatValue = $('.responsiveFlag').css("float");
+  if (floatValue !== undefined) {
+    if (floatValue === 'none') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+jQuery.fn.isMediumDevice = function() {
+  var floatValue = $('.responsiveFlag').css("float");
+  if (floatValue !== undefined) {
+    if (floatValue === 'left') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+jQuery.fn.isLargeDevice = function() {
+  var floatValue = $('.responsiveFlag').css("float");
+  if (floatValue !== undefined) {
+    if (floatValue === 'right') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
