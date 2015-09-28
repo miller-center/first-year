177005062d548e40ba1b7dfe7571d4199588fcc2
Matthew Stephens <mattbastard@gmail.com>
2015-09-28 12:12:50 -0400

    trimming for syntax

--- js/first-year.js
+++ js/first-year.js
@@ -36,7 +36,8 @@ $(document).ready(function() {
           && container.has(e.target).length === 0){ // ... nor a descendant of the container
           container.removeClass('expanded');
       }
-    } });
+    } 
+  });
 
   //
   // Side (Sharing) Bar Animation
@@ -76,78 +77,12 @@ $(document).ready(function() {
 
   }
 
-
-  //
-  // Side (Sharing) Bar Animation
-  //
-  
-  FirstYear.sharingBar = function animateSharingBar(showClass, hideClass, revealPoint) {
-
-    showClass = showClass || "shareBarReveal"; 
-    hideClass = hideClass || "shareBarExit";
-
-    $(window).scroll(function() {
-      var wScroll = Number( $(this).scrollTop() );
-
-      // reveal point for social sharing bar
-      var sbReveal  = revealPoint || Number( $('.hero').height() );
-      var sbConceal = $(document).height() - $('#bodyFooter').height() - ( $(window).height() / 1.05 );
-
-      // slide in social sharing bar from off canvas
-      if (wScroll > sbReveal) {
-        $('#share-bar').addClass(showClass);
-        $('#share-bar').removeClass(hideClass);      
-      } else {
-        $('#share-bar').addClass(hideClass);
-        $('#share-bar').removeClass(showClass);
-      }
-
-      // gracefully exit at footer
-      if (wScroll > sbConceal || wScroll < sbReveal) {
-        $('#share-bar').addClass(hideClass);
-        $('#share-bar').removeClass(showClass);
-      } else {
-        $('#share-bar').addClass(showClass);
-        $('#share-bar').removeClass(hideClass);      
-      }
-
-  // rotate through the list of 'The Latest' headlines
-  function changeNewsItem() {
-    fadeTime = 300;
-    listItems.eq(current++).fadeOut(fadeTime, function() {
-      if (current === length) {
-        current = 0;
-      }
-      listItems.eq(current).fadeIn(fadeTime);
-
-    });
-    return true;
-
-  }
-
-  function rotateNewsItem() {
-    fadeTime = 300;
-    listItems.eq(current++).fadeOut(fadeTime, function() {
-      if (current === length) {
-        current = 0;
-      }
-      listItems.eq(current).fadeIn(fadeTime);      
-    });
-  }
-
-
-
   // if window is large enough do the following:
   //   1. activate newsfeed rotation
   //   2. reveal/conceal sharing sidebar
 
   try {
     if(Foundation.utils.is_medium_up()) {
-      var listItems = $('#the-latest ul li:not(.label)');
-      length = listItems.length;
-      var current = 0;
-      timeout = 5000;
-      setInterval(rotateNewsItem, timeout);
       animateSharingBar();
     } else {
       console.log('some animations disabled for small device');
