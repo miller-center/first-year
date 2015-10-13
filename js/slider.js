1f330e82e001a66cacd080ac18b56ed54b7cb163
Matthew Stephens <mattbastard@gmail.com>
2015-10-13 17:53:49 -0400

    start and stop bound to touch events
    This version is one of two possible implementations,
    the second is in the next commit.
    
    (Hint: use this if you want to touch the ticker to pause, the other
    way will pause on touch anywhere, and toggle behavior.)

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,8 @@ $('.slider').bind('activateTicker', function activateTicker( someIndex ) { // Fo
         $slides = $this.find('.slide'), // Create jQuery object to hold all slides
         buttonArray = [], // Create array to hold navigation buttons
         currentIndex = someIndex || 0, // Hold index number of the current slide
-        timeout; // Sets gap between auto-sliding
+        timeout, // Sets gap between auto-sliding
+        interval = 4000;  // Milliseconds timer will wait
 
     function move(newIndex) { // Creates the slide from old to new one
 
@@ -15,12 +16,13 @@ $('.slider').bind('activateTicker', function activateTicker( someIndex ) { // Fo
             $slides.eq(successor).animate({ opacity: 1   }, 400, "linear");
             $slides.eq(successor).addClass('current');
         });
-
         advance(); // When slide moves, call advance() again
     }
 
-    function advance() { // Used to set 
+    function advance(lastTime) { // Used to set 
         clearTimeout(timeout); // Clear previous timeout
+        FirstYear.timeout = null;
+
         timeout = setTimeout(function() { // Set new timer
             if (currentIndex < ($slides.length - 1)) { // If slide < total slides
                 move(currentIndex++); // Move to next slide and increment counter
@@ -28,8 +30,9 @@ $('.slider').bind('activateTicker', function activateTicker( someIndex ) { // Fo
                 move(currentIndex); // Process the last slide
                 currentIndex = 0; // reset counter
             }
-        }, 4000); // Milliseconds timer will wait
+        }, interval); 
         FirstYear.timeout = timeout;
+
     }
 
     advance(); // Script is set up, advance() to move it
@@ -43,7 +46,9 @@ $(document).ready(function() {
     $('#stopSlide').mouseenter(function() {
         console.log('pausing ticker (timeout = ' + FirstYear.timeout + ')');
         clearTimeout(FirstYear.timeout);
+        FirstYear.timeout = null;
     });
+
 
     $('#stopSlide').mouseleave(function() {
         console.log('resuming ticker...');
@@ -53,4 +58,33 @@ $(document).ready(function() {
         index = currentSlide.index('.slider li.slide');
         $('.slider').trigger('activateTicker', index);
     });
+
+    // start/stop for touch devices
+    $(document).on("touchstart", function (e) {
+        var container = $('#stopSlide');
+        console.log('touchstart detected');
+
+        // if the target of the click isn't the container...
+        if ( container.is(e.target) && container.has(e.target).length === 0) {
+            // stop the ticker
+            console.log('touchstart detected in slider');
+            console.log('pausing ticker (timeout = ' + FirstYear.timeout + ')');
+            clearTimeout(FirstYear.timeout);
+            FirstYear.timeout = null;
+
+        } else {
+            console.log('touchstart detected outside');
+            // check to see if ticker's stoped, restart if it is
+            console.log('FirstYear.timeout='+ FirstYear.timeout);
+            if (FirstYear.timeout === null) {
+                console.log('restarting ticker');
+                currentSlide = $('.slider li.slide').filter('.current');
+                index = currentSlide.index('.slider li.slide');
+                $('.slider').trigger('activateTicker', index);
+            }
+
+        }
+
+    });
+
 });
