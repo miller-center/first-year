5be34b356a379439cde94161cfe06fb897411456
Matthew Stephens <mattbastard@gmail.com>
2015-10-13 15:34:53 -0400

    start/stop added

--- js/slider.js
+++ js/slider.js
@@ -1,83 +1,56 @@
-$('.slider').each(function() { // For every slider
+$('.slider').bind('activateTicker', function activateTicker( someIndex ) { // For every slider
     var $this = $(this), // Current slider
         $group = $this.find('.slide-group'), // Get the slide-group (container)
         $slides = $this.find('.slide'), // Create jQuery object to hold all slides
         buttonArray = [], // Create array to hold navigation buttons
-        currentIndex = 0, // Hold index number of the current slide
+        currentIndex = someIndex || 0, // Hold index number of the current slide
         timeout; // Sets gap between auto-sliding
 
     function move(newIndex) { // Creates the slide from old to new one
-        var animateLeft, slideLeft; // Declare variables
+
+        successor = newIndex < ($slides.length - 1) ? newIndex + 1 : 0
+        
+        $slides.eq(newIndex).removeClass('current');
+        $slides.eq(newIndex).animate({ opacity: 0 }, 400, "linear", function() {
+            $slides.eq(successor).animate({ opacity: 1   }, 400, "linear");
+            $slides.eq(successor).addClass('current');
+        });
 
         advance(); // When slide moves, call advance() again
-
-        // If it is the current slide / animating do nothing
-        if ($group.is(':animated') || currentIndex === newIndex) {
-            return;
-        }
-
-        buttonArray[currentIndex].removeClass('active'); // Remove class from item
-        buttonArray[newIndex].addClass('active'); // Add class to new item
-
-        invisible = 0;
-        visible = 1;
-
-        // if (newIndex > currentIndex) { // If new item > current
-        //     slideLeft = 'green'; // Sit the new slide to the right
-        //     animateLeft = 'red'; // Animate the current group to the left
-        // } else { // Otherwise
-        //     slideLeft = 'yellow'; // Sit the new slide to the left
-        //     animateLeft = 'blue'; // Animate the current group to the right
-        // }
-        // Position new slide to left (if less) or right (if more) of current
-        // $slides.eq(newIndex).css({
-        //     backgroundColor: slideLeft,
-        // });
-
-        // $group.animate({
-        //     color: animateLeft
-        // }, 400, "linear", function() { // Animate slides and
-        //     $slides.eq(currentIndex).css({
-        //         opacity: invisible
-        //     }); // Hide previous slide      
-        //     $slides.eq(newIndex).css({
-        //         opacity: visible
-        //     }); // Set position of the new item
-        //     // $group.css({
-        //     //     opacity: 1
-        //     // }); // Set position of group of slides
-        //     currentIndex = newIndex; // Set currentIndex to the new image
-        // });
-
-        $slides.eq(currentIndex).animate({ opacity: invisible }, 400, "linear", function() {
-            $slides.eq(newIndex).animate({ opacity: visible   }, 400, "linear");
-        });
     }
 
     function advance() { // Used to set 
         clearTimeout(timeout); // Clear previous timeout
         timeout = setTimeout(function() { // Set new timer
             if (currentIndex < ($slides.length - 1)) { // If slide < total slides
-                move(currentIndex + 1); // Move to next slide
+                move(currentIndex++); // Move to next slide and increment counter
             } else { // Otherwise
-                move(0); // Move to the first slide
+                move(currentIndex); // Process the last slide
+                currentIndex = 0; // reset counter
             }
         }, 4000); // Milliseconds timer will wait
+        FirstYear.timeout = timeout;
     }
-
-    $.each($slides, function(index) {
-        // Create a button element for the button
-        var $button = $('<button type="button" class="slide-btn">&bull;</button>');
-        if (index === currentIndex) { // If index is the current item
-            $button.addClass('active'); // Add the active class
-        }
-        $button.on('click', function() { // Create event handler for the button
-            move(index); // It calls the move() function
-        }).appendTo('.slide-buttons'); // Add to the buttons holder
-        buttonArray.push($button); // Add it to the button array
-    });
 
     advance(); // Script is set up, advance() to move it
 
 
+});
+
+$(document).ready(function() {
+    $('.slider').trigger('activateTicker');
+
+    $('#stopSlide').mouseenter(function() {
+        console.log('pausing ticker (timeout = ' + FirstYear.timeout + ')');
+        clearTimeout(FirstYear.timeout);
+    });
+
+    $('#stopSlide').mouseleave(function() {
+        console.log('resuming ticker...');
+        if (FirstYear.timeout) clearTimeout(FirstYear.timeout);
+
+        currentSlide = $('.slider li.slide').filter('.current');
+        index = currentSlide.index('.slider li.slide');
+        $('.slider').trigger('activateTicker', index);
+    });
 });
